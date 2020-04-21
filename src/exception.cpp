ab012f41d024937725c4f01e86206fb93bba35f0
Chris Liu <chrisliu1999@gmail.com>
2020-04-20 20:53:58 -0700

    Removed exception name from message.

--- src/exception.cpp
+++ src/exception.cpp
@@ -1,17 +1,12 @@
 #include "exception.hpp"
 
 void raise_exception(exception_t type, std::string msg) {
-    std::stringstream ss;
-    ss << "(";
     switch (type) {
         case exception_t::LEX:
-            ss << "Lexing" << " Exception) " << msg;
-            throw LexException(ss.str());
+            throw LexException(msg);
         case exception_t::PARSE:
-            ss << "Runtime" << " Exception) " << msg; 
-            ss << "Parse" << " Exception) " << msg; 
-            throw ParseException(ss.str());
+            throw ParseException(msg);
         case exception_t::RUNTIME:
-            throw RuntimeException(ss.str());
+            throw RuntimeException(msg);
     }
 }
--- src/exception.hpp
+++ src/exception.hpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <string>
 #include <exception>
-#include <sstream>
 
 enum exception_t {
     LEX, PARSE, RUNTIME
